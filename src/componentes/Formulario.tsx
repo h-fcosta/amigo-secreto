98aa29760789220f764c9684d353b24e50e27fc3
Henrique Costa <henriquefcosta19@gmail.com>
2022-08-20 20:10:59 -0300

    finalização do curso

---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,42 @@
+import { useRef, useState } from "react";
+import { useAdicionarParticipante } from "../state/hooks/useAdicionarParticipante";
+import { useMensagemDeErro } from "../state/hooks/useMensagemdDeErro";
+import "./Formulario.css";
+
+const Formulario = () => {
+  const [nome, setNome] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const adicionarNaLista = useAdicionarParticipante();
+
+  const mensagemDeErro = useMensagemDeErro();
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    adicionarNaLista(nome);
+    setNome("");
+    inputRef.current?.focus();
+  };
+
+  return (
+    <form onSubmit={adicionarParticipante}>
+      <div className="grupo-input-btn">
+        <input
+          ref={inputRef}
+          value={nome}
+          onChange={(evento) => setNome(evento.target.value)}
+          placeholder="Insira os nomes dos participantes"
+        />
+        <button disabled={!nome}>Adicionar</button>
+      </div>
+      {mensagemDeErro && (
+        <p className="alerta erro" role="alert">
+          {mensagemDeErro}
+        </p>
+      )}
+    </form>
+  );
+};
+
+export default Formulario;
